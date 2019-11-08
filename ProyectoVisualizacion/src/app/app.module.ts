12d0cc4b548684b845a5f8769199a7f04da1a2b2
Kendall Tames <tamesfern@gmail.com>
2019-11-08 13:18:03 -0600

    Updated and added node packages

--- ProyectoVisualizacion/src/app/app.module.ts
+++ ProyectoVisualizacion/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { MainWindowComponent } from './main-window/main-window.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
