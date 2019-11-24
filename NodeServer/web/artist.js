257caed54019ed3a29ff98e1162e305eec0ea2d3
Kendall Tames <tamesfern@gmail.com>
2019-11-24 14:04:40 -0600

    Finished artist scrapper

--- NodeServer/web/artist.js
+++ NodeServer/web/artist.js
@@ -1,7 +1,7 @@
 module.exports = function (axios, cheerio) {
     return {
         getArtist: function (artist_url, res) {
-            const url = "https://musicbrainz.org/" + artist_url;
+            const url = "https://musicbrainz.org" + artist_url;
             
             console.log(url);
             axios(url)
@@ -9,14 +9,29 @@ module.exports = function (axios, cheerio) {
                     // res.send({url: response.toString()});
                     const html = response.data;
                     const $ = cheerio.load(html);
-                    const datos = $('.area').text();
-                    console.log({text: datos});
-                    res.send({area: datos});
+                    const area = $('.area').text();
+                    const tipo = $('.type').text();
+                    const rating = $('.current-rating').first().text();
+                    const link = $('.external_links a').first().attr('href');
+
+                    const table = $('table.release-group-list').first();
+                    const albums = [];
+                    table.find('tbody tr').each(function() {
+                        albums.push({
+                            year: $(this).find('td').first().text(),
+                            releases: $(this).find('td').last().text()
+                        });
+                    });
+
+                    while (albums.length > 3)
+                        albums.shift();
+                    
+                    res.send({area: area, type: tipo, rating: rating, link: link, albums: albums});
 
             //         onSuccess({text: datos});
             //     }, function(error) {
             //         console.log(error);
-                })
+                }, console.log)
         }
     }
 };
